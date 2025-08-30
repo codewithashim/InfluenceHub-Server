899f882239c9414cd8c6fdebe73e99565d17cb33
codewithashim <codewithashim@gmail.com>
2025-08-31 03:35:09 +0600

    influencers issue is fixed

--- src/modules/influencers/influencers.module.ts
+++ src/modules/influencers/influencers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InfluencersController } from './interface/influencers.controller';
 import { InfluencersService } from './influencers.service';
+import { PrismaModule } from 'src/shared/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [InfluencersController],
   providers: [InfluencersService],
 })
