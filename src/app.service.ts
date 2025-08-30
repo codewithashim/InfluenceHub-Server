5c266cacd543549e6e9c22130a74a6a03bd3e9a6
codewithashim <codewithashim@gmail.com>
2025-08-30 19:46:26 +0600

    Auth module init

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from './shared/database/database.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly dbService: DatabaseService) {}
+
+  async getHello(): Promise<string> {
+    const isDbConnected = await this.dbService.isConnected();
+    const projectName = 'InfluenceHub-Server';
+    return isDbConnected
+      ? `${projectName} - Database Connected`
+      : `${projectName} - Database Not Connected`;
   }
 }
